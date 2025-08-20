53579e9c4ffd308d8a65826a3b9b12b5afb281bb
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2025-08-20 08:32:40 +0530

    Update ResCategory.jsx

--- src/components/ResCategory.jsx
+++ src/components/ResCategory.jsx
@@ -1,24 +1,23 @@
-
 import ItemsList from "./ItemsList";
 
-const ResCategory = ({data,showItems,setshowIndex}) =>{
-    
-    const handleClick = ()=>{
-        setshowIndex();
-    }
-    return(
-        <div className="rescategory-accordian">
-            <div className="accordian-header">
-            <span className="accordian-title" 
-            onClick={()=>handleClick()}
-            >{data.title +" ("+data.itemCards.length+")" }</span>
-            <span>{"⬇️"}</span>
-            </div>
-            <div>
-                {showItems && <ItemsList items={data.itemCards}/>}
-            </div>
-        </div>
-    )
+const ResCategory = ({ data, showItems, setshowIndex }) => {
+  const handleClick = () => {
+    setshowIndex();
+  };
+
+  return (
+    <div className="rescategory-accordian">
+      <div className="accordian-header">
+        <button className="accordian-title" onClick={handleClick}>
+          {data.title + " (" + data.itemCards.length + ")"}
+        </button>
+        <button className="accordian-icon" onClick={handleClick}>
+          ⬇️
+        </button>
+      </div>
+      <div>{showItems && <ItemsList items={data.itemCards} />}</div>
+    </div>
+  );
 };
 
 export default ResCategory;

b0acda7313ca2a0fef67965683cd41970ed3a2c8
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2025-08-20 08:12:06 +0530

    Update ResMenu.jsx

--- src/components/ResMenu.jsx
+++ src/components/ResMenu.jsx
@@ -5,49 +5,53 @@ import ResCategory from "./ResCategory";
 import { useState } from "react";
 
 const ResMenu = () => {
- 
-  const { resId }=useParams();
-  const resMenuInfo=useRestaurantMenu(resId);
-  const [showIndex,setshowIndex]=useState(null);
+  const { resId } = useParams();
+  const resMenuInfo = useRestaurantMenu(resId);
+  const [showIndex, setshowIndex] = useState(null);
 
   if (!resMenuInfo) {
     return <Shimmer />;
   }
 
-  // Extracting safely
-  const info = resMenuInfo[2]?.card?.card?.info;
-  const menu= resMenuInfo[4].groupedCard?.cardGroupMap?.REGULAR?.cards[5]?.card?.card?.itemCards;
- const categories =
+  const info = resMenuInfo?.[2]?.card?.card?.info || null;
+  const menu =
+    resMenuInfo?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[5]?.card?.card
+      ?.itemCards || [];
+  const categories =
     resMenuInfo?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     ) || [];
 
-
-  if (!info || menu.length===0) {
-    return <p>Restaurant info not available</p>;
+  if (!info || menu.length === 0 || categories.length === 0) {
+    return <p>Try another restaurant</p>;
   }
 
   return (
     <div className="menu">
       <h1 className="resmenu-resname">{info.name}</h1>
       <h4 className="resmenu-resdetails">Rating: {info.avgRating}</h4>
-      <h4 className="resmenu-resdetails">Cuisines: {info.cuisines?.join(", ")}</h4>
-      <h4 className="resmenu-resdetails">Delivery Time: {info.sla?.deliveryTime} mins</h4>
-      <h4 className="resmenu-resdetails">Price for two: {info.costForTwoMessage}</h4> 
-      {
-        categories.map((category,index) => {
-          return <ResCategory key={category.card?.card?.title} data={category.card.card} 
-          showItems={index===showIndex ? true:false} 
-          setshowIndex={()=> setshowIndex(index)}
-          />;
-        })
-      }
+      <h4 className="resmenu-resdetails">
+        Cuisines: {info.cuisines?.join(", ")}
+      </h4>
+      <h4 className="resmenu-resdetails">
+        Delivery Time: {info.sla?.deliveryTime} mins
+      </h4>
+      <h4 className="resmenu-resdetails">
+        Price for two: {info.costForTwoMessage}
+      </h4>
 
+      {categories.map((category, index) => (
+        <ResCategory
+          key={category.card?.card?.title || index}
+          data={category.card?.card}
+          showItems={index === showIndex}
+          setshowIndex={() => setshowIndex(index)}
+        />
+      ))}
     </div>
   );
 };
 
 export default ResMenu;
-
